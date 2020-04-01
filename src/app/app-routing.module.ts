4cf9c557f04f323f54ef26012e97287da59e0e2e
soroushby <49848393+soroushby@users.noreply.github.com>
2020-04-01 08:19:44 -0700

    commited

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ProductsComponent } from './products/products.component';
+import { WelcomeComponent } from './welcome/welcome.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'products' , component: ProductsComponent},
+  { path: 'welcome' , component: WelcomeComponent},
+  // { path: '' , component: WelcomeComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/products.service.ts
+++ src/app/products.service.ts
@@ -7,11 +7,11 @@ export class ProductsService {
   veganChocolate: any;
   constructor() {}
   getData() {
-    return (this.veganChocolate = [
+    return this.veganChocolate = [
       { name: "mint", price: "34", code: "135" },
       { name: "hazelnut", price: "10", code: "357" },
       { name: "peanut", price: "13", code: "945" },
       { name: "dark", price: "14", code: "385" }
-    ]);
+    ];
   }
 }
