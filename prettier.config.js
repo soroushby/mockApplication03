d98d400754b5a0b857f75a56415cc436f0d81686
Farhad Bayanati <fbayanati@yahoo.com>
2020-04-12 22:14:57 -0700

    format by prettier

--- prettier.config.js
+++ prettier.config.js
@@ -9,6 +9,5 @@ module.exports = {
     singleQuote: false,
     trailingComma: "none", // other options `es5` or `all`
     bracketSpacing: true,
-    arrowParens: "avoid", // other option 'always'
-    parser: "typescript"
+    arrowParens: "avoid" // other option 'always'
 };
