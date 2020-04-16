846b22e4d133e933e85a97c7f65578e06b2f9247
soroushby <49848393+soroushby@users.noreply.github.com>
2020-04-16 03:22:35 -0700

    updated

---
+++ src/app/industrial-design/industrial-design.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { IndustrialDesignComponent } from './industrial-design.component';
+
+describe('IndustrialDesignComponent', () => {
+  let component: IndustrialDesignComponent;
+  let fixture: ComponentFixture<IndustrialDesignComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ IndustrialDesignComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(IndustrialDesignComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/industrial-design/industrial-design.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-industrial-design',
+  templateUrl: './industrial-design.component.html',
+  styleUrls: ['./industrial-design.component.scss']
+})
+export class IndustrialDesignComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
