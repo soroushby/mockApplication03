ab5bf8bad41b5d7fa05b49d60fc7d9234e360750
soroushby <49848393+soroushby@users.noreply.github.com>
2020-04-18 22:55:50 -0700

    formatted

--- src/app/featureModules/auth/auth.module.ts
+++ src/app/featureModules/auth/auth.module.ts
@@ -1,28 +1,23 @@
-import { RouterModule } from '@angular/router';
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { SignupComponent } from 'src/app/signup/signup.component';
-import { LoginComponent } from 'src/app/login/login.component';
-import { MaterialModule } from 'src/app/material/material.module';
-import { FlexLayoutModule } from '@angular/flex-layout';
-import { ReactiveFormsModule } from '@angular/forms';
-
-
+import { RouterModule } from "@angular/router";
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { SignupComponent } from "src/app/signup/signup.component";
+import { LoginComponent } from "src/app/login/login.component";
+import { MaterialModule } from "src/app/material/material.module";
+import { FlexLayoutModule } from "@angular/flex-layout";
+import { ReactiveFormsModule } from "@angular/forms";
 
 @NgModule({
-  declarations: [
-    SignupComponent,
-     LoginComponent,
-  ],
-  imports: [
-    CommonModule,
-    RouterModule.forChild([
-      { path: "signup", component: SignupComponent },
-      { path: "login", component: LoginComponent },
-    ]),
-    MaterialModule,
-    FlexLayoutModule,
-    ReactiveFormsModule,
-  ]
+    declarations: [SignupComponent, LoginComponent],
+    imports: [
+        CommonModule,
+        RouterModule.forChild([
+            { path: "signup", component: SignupComponent },
+            { path: "login", component: LoginComponent }
+        ]),
+        MaterialModule,
+        FlexLayoutModule,
+        ReactiveFormsModule
+    ]
 })
-export class AuthModule { }
+export class AuthModule {}
